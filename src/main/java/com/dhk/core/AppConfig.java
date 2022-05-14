94711444aa4d00086348e752b8a45422a125c959
ekgp4949 <ekgp4949@gmail.com>
2022-05-14 11:21:20 +0900

    AppConfig 리팩토링

--- src/main/java/com/dhk/core/AppConfig.java
+++ src/main/java/com/dhk/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.dhk.core;
 
+import com.dhk.core.discount.DiscountPolicy;
 import com.dhk.core.discount.FixDiscountPolicy;
+import com.dhk.core.member.MemberRepository;
 import com.dhk.core.member.MemoryMemberRepository;
 import com.dhk.core.member.MemberService;
 import com.dhk.core.member.MemberServiceImpl;
@@ -9,12 +11,19 @@ import com.dhk.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 
 }
